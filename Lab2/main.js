3d69b1da811628171b2d4b9980d1b85a739da213
Katarzyna <kasiakadziolka@gmail.com>
2022-10-25 17:00:45 +0200

    Remove comments

--- Lab2/main.js
+++ Lab2/main.js
@@ -47,63 +47,6 @@ function start() {
     window.addEventListener("click", onWindowClick)
 }
 
-// let slideIndex = 0;
-
-
-// function showSlides() {
-//   let i;
-//   const slider = mainContainer.querySelector('#slider')
-//   const slides = slider.querySelector('#slides')
-//   let imgs = slides.querySelectorAll('img')
-//   let dots = document.getElementsByClassName("dot");
-// //   for (i = 0; i < imgs.length; i++) {
-// //     imgs[i].style.display = "none";
-// // //   }
-//   slideIndex++;
-//   if (slideIndex > imgs.length) {slideIndex = 1}
-//   for (i = 0; i < dots.length; i++) {
-//     dots[i].className = dots[i].className.replace(" active", "");
-//   }
-// //   imgs[slideIndex-1].style.display = "block";
-//   dots[slideIndex-1].className += " active";
-//   setTimeout(showSlides, 2000); // Change image every 2 seconds
-// }
-
-// function onLeftButtonClick() {
-
-// }
-// function onRightButtonClick() {
-    
-// }
-// function onStopButtonClick() {
-    
-// }
-
-
-// const timeoutRef = setTimeout( 
-//     () => {
-        
-//     },
-//     2000
-// )
-// let licznik = 0 
-// const intervalRef = setInterval( 
-//     () => {
-//         main.innerHTML='From interval' + licznik++
-//     },
-//     2000
-// )
-
-// //kasujemy setInterval
-// clearInterval(intervalRef)
-
-// //kasujemy setTimeout
-// clearTimeout(intervalRef)
-
-
-// window.requestAnimationFrame()
-
-
 let counter = 0;
 
 
