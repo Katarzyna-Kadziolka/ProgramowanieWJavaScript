8a7a6399e284fe8e4b0deb4e01ac6c1459930591
Katarzyna <kasiakadziolka@gmail.com>
2022-11-27 15:39:45 +0100

    version with promises

--- Lab5/main.js
+++ Lab5/main.js
@@ -9,19 +9,24 @@ const asyncAdd = async (a,b) => {
     })
 }
 
-const GetSum = async(a, b, ...args) => {
-    let sum = await asyncAdd(a, b)
+const GetSum = async(...args) => {
+    let asyncOperations = 0
+    const allPromises = []
+    let sum = 0
     for (const arg of args) {
-        sum = await asyncAdd(sum, arg)
+        allPromises.push(asyncAdd(sum, arg).then((result) => sum += result))
+        asyncOperations = asyncOperations + 1
     }
-    console.log(sum)
+    await Promise.allSettled(allPromises)
+    console.log(`Sum: ${sum}`)
+    console.log(`Number of operations: ${asyncOperations}`)
 }
 
 const getPerformanceTime = async () => {
     const t0 = performance.now()
     await GetSum(15, 22, 40, 3, 8, 21, 5, 13, 6, 9, 15, 22, 40, 3, 8, 21, 5, 13, 6, 9, 15, 22, 40, 3, 8, 21, 5, 13, 6, 9, 15, 22, 40, 3, 8, 21, 5, 13, 6, 9, 
         15, 22, 40, 3, 8, 21, 5, 13, 6, 9, 15, 22, 40, 3, 8, 21, 5, 13, 6, 9, 15, 22, 40, 3, 8, 21, 5, 13, 6, 9, 15, 22, 40, 3, 8, 21, 5, 13, 6, 9, 
-        15, 22, 40, 3, 8, 21, 5, 13, 6, 9, 15, 22, 40, 3, 8, 21, 5, 13, 6, 9, )
+        15, 22, 40, 3, 8, 21, 5, 13, 6, 9, 15, 22, 40, 3, 8, 21, 5, 13, 6, 9)
     const t1 = performance.now()
     console.log(`Time: ${t1 - t0}`)
 }
