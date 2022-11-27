e5ab900b24c1232ad3685469f5991a498e3ac21f
Katarzyna <kasiakadziolka@gmail.com>
2022-11-27 14:43:07 +0100

    Get sum

--- Lab5/main.js
+++ Lab5/main.js
@@ -1,15 +1,20 @@
-const asyncAdd = async (a,b, ...args) => {
+const asyncAdd = async (a,b) => {
     if (typeof a !== 'number' || typeof b !== 'number') {
       return Promise.reject('Argumenty muszą mieć typ number!')
-    }
-    for (const arg of args) {
-        if(typeof arg !== 'number') {
-            return Promise.reject('Argumenty muszą mieć typ number!')
-        }
     }
     return new Promise((resolve, reject) => {
       setTimeout(() =>{
         resolve(a+b)
       }, 100)
     })
-  }
+}
+
+const GetSum = async(a, b, ...args) => {
+    let sum = await asyncAdd(a, b)
+    for (const arg of args) {
+        sum = await asyncAdd(sum, arg)
+    }
+    console.log(sum)
+}
+
+GetSum(10, 20, 1, 55, 68, 79)
