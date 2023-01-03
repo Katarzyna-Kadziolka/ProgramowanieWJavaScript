985198cadc4ebaaaeb1a3dfc5b9c54a370d1f614
Katarzyna <kasiakadziolka@gmail.com>
2023-01-03 12:31:02 +0100

    More circles

--- Lab6/main.js
+++ Lab6/main.js
@@ -5,13 +5,8 @@ let ctx
 const canvaWidth = 1200
 const canvaHeigh = 500
 const circles = new Array();
-const radius = 20 
 const startAngle = 0 
 const endAngle = Math.PI * 2 
-let x = 600 
-let y = 250
-let dx = 2
-let dy = 4
 
 function start() {
     const startButton = document.querySelector('#startButton')
@@ -22,45 +17,57 @@ function start() {
 const onStartClick = () => {
     canvas = document.getElementById('mainCanvas')
     if (!canvas.getContext) {
-        throw new Error('Brak f. canvas.getContext');
+        throw new Error('Brak f. canvas.getContext')
     }
-    ctx = canvas.getContext('2d');
+    ctx = canvas.getContext('2d')
 
-    createCircle()
+    for (var i = 0; i < 10; i++) {
+        const radius = Math.random() * 30 + 10
+        let x = Math.random() * canvas.width
+        let y = Math.random() * canvas.height
+        while (x > canvas.width-radius || x < radius) {
+            x = Math.random() * canvas.width
+        }
+        while (y > canvas.height-radius || y < radius) {
+            y = Math.random() * canvas.height
+        }
+        
+        let dx = Math.random() * 10
+        let dy = Math.random() * 10
+        circles.push({x: x, y: y, radius: radius, dx: dx, dy: dy})
+    }
+
     animate();
   
 }
 
-const createCircle = () => {
+const drawCircle = (circle) => {
     ctx.beginPath()
 
-    circles.push({
-        x: x,
-        y: y,
-    })
-
-    ctx.arc(x, y, radius, startAngle, endAngle)
+    ctx.arc(circle.x, circle.y, circle.radius, startAngle, endAngle)
 
     ctx.fillStyle = "#FFA500"
     ctx.fill()
     ctx.save()
 }
 
-const updatePosition = () => {
-    x += dx
-    y += dy
+const updatePosition = (circle) => {
+    circle.x += circle.dx
+    circle.y += circle.dy
 
-    if (x + radius > canvas.width || x - radius < 0) {
-        dx = -dx;
+    if (circle.x + circle.radius > canvas.width || circle.x - circle.radius < 0) {
+        circle.dx = -circle.dx;
     }
-    if (y + radius > canvas.height || y - radius < 0) {
-        dy = -dy;
+    if (circle.y + circle.radius > canvas.height || circle.y - circle.radius < 0) {
+        circle.dy = -circle.dy;
     }
 }
 
 const animate = () => {
-    ctx.clearRect(0, 0, canvaWidth, canvaHeigh);
-    createCircle(ctx);
-    updatePosition(20, 20);
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    for (var i = 0; i < circles.length; i++) {
+        drawCircle(circles[i]);
+        updatePosition(circles[i]);
+      }
     requestAnimationFrame(animate);
 }
