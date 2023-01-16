88b25d9056ed3689cf5e8bfd72c85aab443eeb5d
Katarzyna <kasiakadziolka@gmail.com>
2023-01-16 17:15:08 +0100

    Sledzik version

--- Lab7/main.js
+++ Lab7/main.js
@@ -1,56 +1,78 @@
 document.addEventListener("DOMContentLoaded", start)
 
+let addButton
+let weatherInfos = []
+
 function start() {
-    const addButton = document.querySelector('#addButton')
+    addButton = document.querySelector('#addButton')
     addButton.addEventListener("click", onAddButtonClick)
     const saveButton = document.querySelector('#save-button')
     saveButton.addEventListener("click", save)
 
-
     load()
 }
 
-const apiKey = '908d7a4b33c9430baa0d5f75a374e8ab';
 const cityName = 'London';
+const key = '908d7a4b33c9430baa0d5f75a374e8ab'
+const url = `http://api.openweathermap.org/data/3.0/onecall?`;
 
-const url = `http://api.openweathermap.org/geo/1.0/direct?limit=5&lang=pl&q=${cityName}&limit=10&appid=${apiKey}`;
+const getCoordinates = (cityName) => {
+    return fetch('http://api.openweathermap.org/geo/1.0/direct?q=' + cityName + '&limit=1&appid=' + key)  
+    .then(function(resp) { return resp.json() })
+    .then(function(data) {
+      return {
+        longitude: data[0].lon,
+        latitiude: data[0].lat
+      }
+    })
+    .catch(function() {
+    });
+}
 
-const getData  = fetch (url, { headers: { 'Content-Type': 'application/json' } })
-        .then(response => response.json())
-        .then(data => {
-            data.forEach(city => {
-            console.log(`City: ${city.name}, Country: ${city.country}`);
-            });
-        })
-        .catch(error => console.error(error));
+const getData = (lat, lon) => {
+    return fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${key}`)  
+    .then(function(resp) { return resp.json() })
+    .then(function(data) {
+      return data;
+    })
+    .catch(function() {
+    });
+  }
 
 
 const onAddButtonClick = () => {
-    addNewCard()
+    const selectCity = document.querySelector('#city')
+    const cityName = selectCity.value
+    weatherInfos.push(cityName)
+    addNewCard(cityName, weatherInfos.length)
+    handleButtonState()
 }
 
-let weatherInfos = []
-const addNewCard = () => {
-    const response = getData()
+const addNewCard = async (cityName, index) => {
+    const coordinate = await getCoordinates(cityName)
+    const response = await getData(coordinate.latitiude, coordinate.longitude)
     const weatherInfo = new WheatherInfo()
-    const timeElapsed = Date.now()
-    const today = new Date(timeElapsed)
-    weatherInfo.LastUpdate = today.toDateString()
-    createCard(weatherInfo, weatherInfos.length)
-    weatherInfos.push(weatherInfo)
+    weatherInfo.City = cityName
+    weatherInfo.Temperature = response.main.temp
+    weatherInfo.Humidity = response.main.humidity
+    weatherInfo.Main = response.weather[0].icon
+
+    createCard(weatherInfo, index)
 }
 
-const createCard = (note, index) => {
+const createCard = (weatherInfo, index) => {
     const newCard = document.createElement('div')
     newCard.classList.add("note");
     newCard.innerHTML = `<div>
         <div class="note-header">
             <span id="city" class="city"></span>
         </div>
-        <span id="date" class="date"></span>
         <hr>
-        <span id="temperature"></span>
+        <span id="temperature"></span><br>
         <span id="humidity"></span>
+        <div>
+            <img id="weatherImage" src="" alt="Weather">
+        </div>
     </div>
         <div class="color-picker-container">
             <button id="delete-button" class="save-button">Delete</button>
@@ -58,12 +80,20 @@ const createCard = (note, index) => {
 
 
     newCard.querySelector('#delete-button').addEventListener('click', () => deleteNote(index))
-    
-    newCard.querySelector('#date').value = note.Date
+
+    newCard.querySelector('#city').textContent = weatherInfo.City
+    newCard.querySelector('#temperature').textContent = 'Temperatura: ' + toCelcius(weatherInfo.Temperature) + '℃'
+    newCard.querySelector('#humidity').textContent = 'Wilgotność: ' + weatherInfo.Humidity + '%'
+
+    newCard.querySelector("#weatherImage").src=`http://openweathermap.org/img/wn/${weatherInfo.Main}@2x.png`
 
     const main = document.getElementById('note-container')
     main.appendChild(newCard)
 
+}
+
+const toCelcius = (kelvinTemperature) => {
+    return Math.round(kelvinTemperature - 272.15)
 }
 
 const save = () => {
@@ -78,10 +108,8 @@ const load = () => {
     }
     weatherInfos = savedNotes
 
-    let index = 0
-    for (const note of weatherInfos) {
-        createCard(note, index)
-        index++;
+    for (let index = 0; index < weatherInfos.length; index++) {
+        addNewCard(weatherInfos[index], index)
     }
 }
 
@@ -90,6 +118,7 @@ const deleteNote = (index) => {
     clearBoard()
     save()
     load()
+    handleButtonState()
 }
 
 const clearBoard = () => {
@@ -97,9 +126,21 @@ const clearBoard = () => {
     main.textContent = ''
 }
 
+const onWeatherInfosPush = () => {
+    handleButtonState()
+}
+
+const handleButtonState = () => {
+    if(weatherInfos.length == 10) {
+        addButton.disabled = true
+    }
+    else {
+        addButton.disabled = false
+    }
+}
+
 class WheatherInfo {
     City = ''
-    LastUpdate = ''
     Temperature = 0
     Humidity = 0
     Main = ''
